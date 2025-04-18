073731e8ac878d54fdc8a95bc529ad7ca20d460e
samarth <gambhirsamarth@gmail.com>
2025-04-18 16:33:07 +0530

    Add Pokemon Feature

--- main/utility/InputHandler.go
+++ main/utility/InputHandler.go
@@ -1,6 +1,9 @@
 package utility
 
-import "fmt"
+import (
+	"GoDex/main/model"
+	"fmt"
+)
 
 func RequestUserCommand() (command string) {
 	fmt.Println()
@@ -13,5 +16,49 @@ func RequestPokemonName() (pokemonName string) {
 	fmt.Println()
 	fmt.Println("Please enter the Pokemon name: ")
 	fmt.Scanln(&pokemonName)
-	return pokemonName
+	return
+}
+
+func RequestPokemonType() (pokemonType string) {
+	fmt.Println()
+	fmt.Println("Please enter the Pokemon type: ")
+	fmt.Scanln(&pokemonType)
+	return
+}
+
+func RequestPokemonHP() (pokemonHP int) {
+	fmt.Println()
+	fmt.Println("Please enter the Pokemon HP: ")
+	fmt.Scanln(&pokemonHP)
+	return
+}
+
+func RequestPokemonAttack() (pokemonAttack int) {
+	fmt.Println()
+	fmt.Println("Please enter the Pokemon Attack: ")
+	fmt.Scanln(&pokemonAttack)
+	return
+}
+
+func RequestPokemonDefense() (pokemonDefense int) {
+	fmt.Println()
+	fmt.Println("Please enter the Pokemon Defense: ")
+	fmt.Scanln(&pokemonDefense)
+	return
+}
+
+func RequestPokemon() (pokemon model.Pokemon) {
+	name := RequestPokemonName()
+	pokeomonType := RequestPokemonType()
+	hp := RequestPokemonHP()
+	attack := RequestPokemonAttack()
+	defense := RequestPokemonDefense()
+	pokemon = model.Pokemon{
+		Name: name,
+		Type: pokeomonType,
+		HP: hp,
+		Attack: attack,
+		Defense: defense,
+	}
+	return
 }
